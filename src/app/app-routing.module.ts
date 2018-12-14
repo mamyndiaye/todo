e50620affe4807171df59ab2f12f97c84fb3b319
Ndiaye <ndeyewanendiaye@gmail.com>
2018-12-14 01:18:51 +0000

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{AppRoutingModule} from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
@@ -8,32 +8,35 @@ import { BookListComponent } from './book-list/book-list.component';
 import { SingleBookComponent } from './book-list/single-book/single-book.component';
 import { BookFormComponent } from './book-list/book-form/book-form.component';
 import { HeaderComponent } from './header/header.component';
-import { from } from 'rxjs/internal/observable/from';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import { from } from 'rxjs/internal/observable/from';
 import {AuthService} from "./services/auth.service";
 import { BooksService} from "./services/books.service";
 import {AuthGuardService} from "./services/auth-guard.service";
-@NgModule({
 
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    SigninComponent,
-    BookListComponent,
-    SingleBookComponent,
-    BookFormComponent,
-    HeaderComponent
-  ],
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'books', canActivate: [AuthGuardService], component: BookListComponent },
+  { path: 'books/new', canActivate: [AuthGuardService], component: BookFormComponent },
+  { path: 'books/view/:id', canActivate: [AuthGuardService], component: SingleBookComponent },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: '**', redirectTo: 'books' }
+];
+
+@NgModule({
+  declarations: [],
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+   RouterModule.forRoot(appRoutes)
   ],
-  providers: [AuthService, BooksService, AuthGuardService],
-  bootstrap: [AppComponent]
+  exports: [ RouterModule ]
 })
-export class AppModule { }
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+       title = 'todolist';
+  
+       constructor() {
+ const  config = {
+    apiKey: "AIzaSyDCdJe8pBhw7yhdxWR7eS8aCqohD4pLa5g",
+    authDomain: "todo-3180e.firebaseapp.com",
+    databaseURL: "https://todo-3180e.firebaseio.com",
+    projectId: "todo-3180e",
+    storageBucket: "todo-3180e.appspot.com",
+    messagingSenderId: "836231388449"
+  };
+  firebase.initializeApp(config);
+
+}
 }
